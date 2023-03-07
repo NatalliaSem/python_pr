c2b1cf0cf28c4afbfc37c319e19574c16e62f8f2
Natallia Semianiuk <natallia_semianiuk@epam.com>
2023-03-07 01:25:58 +0100

    fixed

--- Home_task3.py
+++ Home_task3.py
@@ -14,9 +14,15 @@ last_words= re.findall(r"\s(\w+)\.\s+", text) #find all last words
 
 all_words=' '.join(last_words) #union them
 
-new_sentence=f'\tlist of last words for new sentence: {all_words}.\n' #create new sentence
+new_sentence=f'\n\tlist of last words for new sentence: {all_words}.' #create new sentence
 
-new_text=text +new_sentence #add new sentence to the end of the text
+#new_text=text +new_sentence #add new sentence to the end of the text
+
+position= re.search(r"this paragraph.", text).end() #find position of the end of paragraph
+#print(position)
+
+new_text=text[:position] + new_sentence +text[position:] #add new sentence to the end of paragraph
+#print(nex_text)
 
 first_words = re.split('(\.\s+)', new_text) #split text on sentence
 
@@ -24,13 +30,13 @@ upper_words=[word.capitalize() for word in first_words] #capitalize first letter
 
 update_text ="".join(upper_words) #union all parts
 
-final_text= update_text.replace(' iz', ' is') #replace 'iz' by 'is'
+final_text= update_text.replace(' iz ', ' is ') #replace 'iz' by 'is'
 
-d=0 #calculate number of whitespace
-for i in final_text:
-    if i.isspace():
-        d +=1
+# d=0 #calculate number of whitespace
+# for i in final_text:
+#     if i.isspace():
+#          d +=1
+d= len(re.findall(r'[\s]', final_text))
 
 print(f'New version:\n{final_text}')
-
 print(f'Number of whitespaces: {d}')
